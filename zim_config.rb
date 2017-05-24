5877a52630e156abff94a76a62182cbb962c9674
Peter Donald <peter@realityforge.org>
2017-05-24 13:21:20 +1000

    Patch buildr versions

--- zim_config.rb
+++ zim_config.rb
@@ -22,7 +22,7 @@ braid_tasks('way_of_stock' => 'vendor/docs/way_of_stock',
 
 ruby_upgrade('2.1.3', '2.3.1')
 
-patch_gem('buildr', %w(1.4.23 1.4.25 1.5.0 1.5.1 1.5.2), '1.5.3')
+patch_gem('buildr', %w(1.4.20 1.4.23 1.4.24 1.4.25 1.5.0 1.5.1 1.5.2), '1.5.3')
 patch_gem('braid', %w(1.0.10 1.0.11 1.0.12 1.0.13 1.0.14 1.0.15 1.0.16 1.0.17 1.0.18 1.0.19), '1.0.20')
 
 require File.expand_path('belt_config.rb')
