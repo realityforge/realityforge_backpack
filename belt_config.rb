4962cfac8455015093170cdfef3942be70952378
Peter Donald <peter@realityforge.org>
2017-08-28 13:32:58 +1000

    Add new project for pluralisight course

--- belt_config.rb
+++ belt_config.rb
@@ -96,6 +96,7 @@ Belt.scope('realityforge') do |o|
   o.project('ohai-system_packages')
 
   # Projects created during learning of new technology
+  o.project('course-creating-reusable-react-components', :description => 'Coursework for "Creating Reusable React Components" course')
   o.project('course-closure-library', :description => 'Coursework for "Closure Library: Build Complex JavaScript Applications" course', :tags => %w(historic))
   o.project('course-advanced-react-and-redux', :description => 'Coursework for Advanced React and Redux course', :tags => %w(historic))
   o.project('course-graphql-with-react', :description => 'Coursework for GraphQL with React course', :tags => %w(historic))
