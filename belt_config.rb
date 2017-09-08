48e1609990cbd503e8f4f095533081151dbc24bc
Peter Donald <peter@realityforge.org>
2017-09-08 23:19:58 +1000

    Add mobx-react-typescript-boilerplate project

--- belt_config.rb
+++ belt_config.rb
@@ -115,6 +115,7 @@ Belt.scope('realityforge') do |o|
   o.project('mobx', :description => 'Simple, scalable state management.', :tags => %w(external homepage=http://mobx.js.org))
   o.project('mobx-react', :description => 'React bindings for MobX', :tags => %w(external))
   o.project('mobx-utils', :description => 'Utility functions and common patterns for MobX', :tags => %w(external))
+  o.project('mobx-react-typescript-boilerplate', :description => 'Minimal boilerplate for a single-page app using React, TypeScript, TSX, Webpack', :tags => %w(external))
   o.project('schmooze', :description => 'Schmooze lets Ruby and Node.js work together intimately.', :tags => %w(external))
   o.project('keycloak', :description => 'Open Source Identity and Access Management For Modern Applications and Services', :tags => %w(external homepage=http://www.keycloak.org))
   o.project('docker-keycloak', :description => 'Docker image for Keycloak project', :tags => %w(external))
