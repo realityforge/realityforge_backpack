d1ffebbddecdf9e103ce9b226ce769f2717d72ba
Peter Donald <peter@realityforge.org>
2017-12-01 21:14:12 +1100

    Historic projects need no travis configuration

--- belt_config.rb
+++ belt_config.rb
@@ -192,8 +192,8 @@ Belt.scope('realityforge') do |o|
   o.project('gwt-ga', :description => 'A simple GWT library for interacting with Google Analytics', :tags => %w(historic))
 
   # Historic: Moved to using a simpler + GWT 3.0 compatible strategy of System.getProperty() instead
-  o.project('gwt-property-source', :description => 'Provides a convenient way of compiling GWT property values into your module.', :tags => %w(travis issues historic))
-  o.project('gwt-property-source-example', :description => 'A sample application that demonstrates the use of the gwt-property-source', :tags => %w(travis issues historic))
+  o.project('gwt-property-source', :description => 'Provides a convenient way of compiling GWT property values into your module.', :tags => %w(issues historic))
+  o.project('gwt-property-source-example', :description => 'A sample application that demonstrates the use of the gwt-property-source', :tags => %w(issues historic))
 
   # Historic: Inlined the code into all downstream libraries
   o.project('simple-session-filter', :description => 'A simple servlet filter for implementing custom session management', :tags => %w(historic))
