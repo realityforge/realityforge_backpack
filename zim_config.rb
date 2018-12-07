8571eec99e548dd312ef2f4bf2fbd8f3ae1d6b73
Peter Donald <peter@realityforge.org>
2018-12-07 22:13:16 +1100

    Patched more gemfile changes

--- zim_config.rb
+++ zim_config.rb
@@ -80,7 +80,7 @@ command(:remove_deprecated_gem_config) do |app|
   puts Dir['*.gemspec']
   Dir['*.gemspec'].each do |filename|
     patched =
-      patch_file(filename) {|content| content.gsub(/\n *spec\.has_rdoc *=[^\n]*\n/m, "\n").gsub(/\n *s\.has_rdoc *=[^\n]*\n/m, "\n")}
+      patch_file(filename) {|content| content.gsub(/\n *spec\.default_executable *=[^\n]*\n/m, "\n").gsub(/\n *s\.default_executable *=[^\n]*\n/m, "\n")}
     mysystem("git commit -m \"Remove deprecated gem method\"") if patched
   end
 end
