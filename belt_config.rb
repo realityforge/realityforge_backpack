3689d6060a211e06464046dabfc091a1d84d4fc2
Peter Donald <peter@realityforge.org>
2019-01-18 21:30:58 +1100

    Rename streak to spritz

--- belt_config.rb
+++ belt_config.rb
@@ -7,7 +7,7 @@ Belt.scope('buidl') do |o|
 end
 
 Belt.scope('spritz') do |o|
-  o.project('streak', :description => 'A browser based, reactive event streaming library that is best used when coordinating events', :tags => %w(travis issues homepage=https://spritz.github.io))
+  o.project('spritz', :description => 'A browser based, reactive event streaming library that is best used when coordinating events', :tags => %w(travis issues homepage=https://spritz.github.io))
   o.project('spritz.github.io', :description => 'Spritz website', :tags => %w(pages homepage=https://spritz.github.io zim=no))
 end
 
