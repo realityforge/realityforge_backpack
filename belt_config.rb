6b5743e9a462fe4a3f78a52e901c834707f07060
Peter Donald <peter@realityforge.org>
2019-02-28 13:26:29 +1100

    Reorganize deprecated projects

--- belt_config.rb
+++ belt_config.rb
@@ -16,16 +16,18 @@ Belt.scope('arez') do |o|
   # TODO: arez.github.io has several custom deploy keys. Figure out a way to automate this?
   o.project('arez.github.io', :description => 'Arez website', :tags => %w(pages homepage=https://arez.github.io zim=no))
   o.project('arez-dom', :description => 'Arez browser components that make DOM properties observable', :tags => %w(travis issues homepage=https://arez.github.io/dom))
-  o.project('arez-idlestatus', :description => 'Arez Browser component that tracks when the user is idle', :tags => %w(historic))
   o.project('arez-mediaquery', :description => 'Arez browser component that exposes when a CSS media query is matched', :tags => %w(travis issues homepage=https://arez.github.io/mediaquery))
   o.project('arez-networkstatus', :description => 'Arez Browser component that tracks when the user is online', :tags => %w(travis issues homepage=https://arez.github.io/networkstatus))
   o.project('arez-browserlocation', :description => 'Arez component for the browser' 's location hash', :tags => %w(travis issues homepage=https://arez.github.io/browserlocation))
   o.project('arez-promise', :description => 'Arez component that wraps a Promise and makes it observable', :tags => %w(travis issues homepage=https://arez.github.io/promise))
-  o.project('arez-ticker', :description => 'Arez component that ticks at a specified interval', :tags => %w(historic))
-  o.project('arez-timeddisposer', :description => 'Arez utility that will dispose specified node after a delay', :tags => %w(historic))
   o.project('arez-spytools', :description => 'Arez utilities that enhance the spy capabilities', :tags => %w(travis issues homepage=https://arez.github.io/spytools))
   o.project('arez-when', :description => 'Arez component that waits until a condition is true and then runs an effect action', :tags => %w(travis issues homepage=https://arez.github.io/when))
   o.project('arez-devtools', :description => 'Browser-based Arez DevTools', :tags => %w(travis issues))
+
+  # Historic projects
+  o.project('arez-idlestatus', :description => 'Arez Browser component that tracks when the user is idle', :tags => %w(historic))
+  o.project('arez-ticker', :description => 'Arez component that ticks at a specified interval', :tags => %w(historic))
+  o.project('arez-timeddisposer', :description => 'Arez utility that will dispose specified node after a delay', :tags => %w(historic))
 
   o.projects.each do |project|
     project.tags << "name=#{project.name}"
@@ -37,10 +39,12 @@ Belt.scope('react4j') do |o|
   # TODO: react4j.github.io has several custom deploy keys. Figure out a way to automate this?
   o.project('react4j.github.io', :description => 'React4j website', :tags => %w(pages homepage=https://react4j.github.io zim=no))
   o.project('react4j-todomvc', :description => 'React4j TodoMVC implementation', :tags => %w(travis homepage=https://react4j.github.io/docs/todomvc.html zim:branches=raw,arez,dagger,raw_maven,arez_maven,dagger_maven,raw_maven_j2cl,arez_maven_j2cl,dagger_maven_j2cl))
-  o.project('react4j-widget', :description => 'React4j interoperability with GWT Widget API', :tags => %w(historic))
   o.project('react4j-windowportal', :description => 'React4j portal that renders into a Window', :tags => %w(travis homepage=https://react4j.github.io/windowportal))
   o.project('react4j-flux-challenge', :description => 'React4j Flux Challenge implementation', :tags => %w(travis))
   o.project('react4j-drumloop', :description => 'A React4j experimental drum machine', :tags => %w(travis))
+
+  # Historic projects
+  o.project('react4j-widget', :description => 'React4j interoperability with GWT Widget API', :tags => %w(historic))
 
   o.projects.each do |project|
     project.tags << "name=#{project.name}"
