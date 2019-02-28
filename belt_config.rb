2e79d471193e06d9abb1a7b4131d77ad33f81ba8
Peter Donald <peter@realityforge.org>
2019-02-28 13:19:56 +1100

    Remove homepage for historic project

--- belt_config.rb
+++ belt_config.rb
@@ -37,7 +37,7 @@ Belt.scope('react4j') do |o|
   # TODO: react4j.github.io has several custom deploy keys. Figure out a way to automate this?
   o.project('react4j.github.io', :description => 'React4j website', :tags => %w(pages homepage=https://react4j.github.io zim=no))
   o.project('react4j-todomvc', :description => 'React4j TodoMVC implementation', :tags => %w(travis homepage=https://react4j.github.io/docs/todomvc.html zim:branches=raw,arez,dagger,raw_maven,arez_maven,dagger_maven,raw_maven_j2cl,arez_maven_j2cl,dagger_maven_j2cl))
-  o.project('react4j-widget', :description => 'React4j interoperability with GWT Widget API', :tags => %w(historic homepage=https://react4j.github.io/widget))
+  o.project('react4j-widget', :description => 'React4j interoperability with GWT Widget API', :tags => %w(historic))
   o.project('react4j-windowportal', :description => 'React4j portal that renders into a Window', :tags => %w(travis homepage=https://react4j.github.io/windowportal))
   o.project('react4j-flux-challenge', :description => 'React4j Flux Challenge implementation', :tags => %w(travis))
   o.project('react4j-drumloop', :description => 'A React4j experimental drum machine', :tags => %w(travis))
