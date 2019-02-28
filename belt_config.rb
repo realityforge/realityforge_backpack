b1c79289baec719f6e34a7a766ccbb4ec7d65528
Peter Donald <peter@realityforge.org>
2019-02-28 13:31:18 +1100

    Deprecate arez-when

--- belt_config.rb
+++ belt_config.rb
@@ -21,13 +21,13 @@ Belt.scope('arez') do |o|
   o.project('arez-browserlocation', :description => 'Arez component for the browser' 's location hash', :tags => %w(travis issues homepage=https://arez.github.io/browserlocation))
   o.project('arez-promise', :description => 'Arez component that wraps a Promise and makes it observable', :tags => %w(travis issues homepage=https://arez.github.io/promise))
   o.project('arez-spytools', :description => 'Arez utilities that enhance the spy capabilities', :tags => %w(travis issues homepage=https://arez.github.io/spytools))
-  o.project('arez-when', :description => 'Arez component that waits until a condition is true and then runs an effect action', :tags => %w(travis issues homepage=https://arez.github.io/when))
   o.project('arez-devtools', :description => 'Browser-based Arez DevTools', :tags => %w(travis issues))
 
   # Historic projects
   o.project('arez-idlestatus', :description => 'Arez Browser component that tracks when the user is idle', :tags => %w(historic))
   o.project('arez-ticker', :description => 'Arez component that ticks at a specified interval', :tags => %w(historic))
   o.project('arez-timeddisposer', :description => 'Arez utility that will dispose specified node after a delay', :tags => %w(historic))
+  o.project('arez-when', :description => 'Arez component that waits until a condition is true and then runs an effect action', :tags => %w(historic))
 
   o.projects.each do |project|
     project.tags << "name=#{project.name}"
