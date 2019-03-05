29fcaa3b26dca9668d20c17e800ee444c5500100
Peter Donald <peter@realityforge.org>
2019-03-06 09:19:43 +1100

    Mark arez-networkstatus as historic as has been merged into arez-dom

--- belt_config.rb
+++ belt_config.rb
@@ -16,7 +16,6 @@ Belt.scope('arez') do |o|
   # TODO: arez.github.io has several custom deploy keys. Figure out a way to automate this?
   o.project('arez.github.io', :description => 'Arez website', :tags => %w(pages homepage=https://arez.github.io zim=no))
   o.project('arez-dom', :description => 'Arez browser components that make DOM properties observable', :tags => %w(travis issues homepage=https://arez.github.io/dom))
-  o.project('arez-networkstatus', :description => 'Arez Browser component that tracks when the user is online', :tags => %w(travis issues homepage=https://arez.github.io/networkstatus))
   o.project('arez-browserlocation', :description => 'Arez component for the browser' 's location hash', :tags => %w(travis issues homepage=https://arez.github.io/browserlocation))
   o.project('arez-promise', :description => 'Arez component that wraps a Promise and makes it observable', :tags => %w(travis issues homepage=https://arez.github.io/promise))
   o.project('arez-spytools', :description => 'Arez utilities that enhance the spy capabilities', :tags => %w(travis issues homepage=https://arez.github.io/spytools))
@@ -25,6 +24,7 @@ Belt.scope('arez') do |o|
   # Historic projects
   o.project('arez-idlestatus', :description => 'Arez Browser component that tracks when the user is idle', :tags => %w(historic))
   o.project('arez-mediaquery', :description => 'Arez browser component that exposes when a CSS media query is matched', :tags => %w(historic))
+  o.project('arez-networkstatus', :description => 'Arez Browser component that tracks when the user is online', :tags => %w(historic))
   o.project('arez-ticker', :description => 'Arez component that ticks at a specified interval', :tags => %w(historic))
   o.project('arez-timeddisposer', :description => 'Arez utility that will dispose specified node after a delay', :tags => %w(historic))
   o.project('arez-when', :description => 'Arez component that waits until a condition is true and then runs an effect action', :tags => %w(historic))
