f1849d72b12528f49f82223b5eeed3fa2edaa8f0
Peter Donald <peter@realityforge.org>
2019-03-24 11:28:22 +1100

    Update description

--- belt_config.rb
+++ belt_config.rb
@@ -71,7 +71,7 @@ Belt.scope('realityforge') do |o|
   o.project('javax.annotation', :description => 'A minimal, J2CL compatible javax.annotation library', :tags => %w(travis))
   o.project('org.jetbrains.annotations', :description => 'A minimal, J2CL compatible org.jetbrains.annotations library', :tags => %w(travis))
 
-  o.project('bazel-depgen', :description => 'Generate Bazel Dependencies from Maven repository', :tags => %w(travis))
+  o.project('bazel-depgen', :description => 'Generate Bazel dependency scripts by traversing Maven repositories', :tags => %w(travis))
 
   o.project('guiceyloops', :description => 'GuiceyLoops is a minimalistic library for aiding the testing of JEE applications using Guice.', :tags => %w(codecov notify:stock travis))
 
