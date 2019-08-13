f45f8d12c6cc12b727fcdd2a154396b6b97d3c58
Peter Donald <peter@realityforge.org>
2019-08-13 11:49:38 +1000

    Record another project

--- scopes/artemis-esf.rb
+++ scopes/artemis-esf.rb
@@ -16,4 +16,5 @@ Belt.scope('artemis-esf') do |o|
   o.project('open3mod', :description => 'Open 3D Model Viewer - A quick and powerful 3D model viewer')
   o.project('noa', :description => 'Experimental voxel game engine.')
   o.project('proxx', :description => 'A game of proximity')
+  o.project('md2-view', :description => 'A MD2 model viewer')
 end
