40c687946c0bc3ed627b0bcf3417697bf5b4a0d5
Peter Donald <peter@realityforge.org>
2019-09-02 10:20:13 +1000

    Add jsonb-datatypes project

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -20,6 +20,7 @@ Belt.scope('realityforge') do |o|
   o.project('mcrt', :description => 'Maven Central Release Tool: A ruby tool to close, promote and drop staging repositories', :tags => %w(travis))
   o.project('javax.annotation', :description => 'A minimal, J2CL compatible javax.annotation library', :tags => %w(travis))
   o.project('org.jetbrains.annotations', :description => 'A minimal, J2CL compatible org.jetbrains.annotations library', :tags => %w(travis))
+  o.project('jsonb-datatypes', :description => 'Some simple jsonb data type support', :tags => %w(travis))
 
   o.project('guiceyloops', :description => 'GuiceyLoops is a minimalistic library for aiding the testing of JEE applications using Guice.', :tags => %w(codecov notify:stock travis))
 
