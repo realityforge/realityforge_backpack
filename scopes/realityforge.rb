e0a89af84f26b1572ccea5520f914bef993e043e
Peter Donald <peter@realityforge.org>
2019-09-04 09:28:13 +1000

    Add course-pcg-roguelike

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -98,6 +98,7 @@ Belt.scope('realityforge') do |o|
   o.project('course-rust-lang', :description => 'Coursework for the  "The Rust Programming Language" Udemy course')
   o.project('course-advanced-css-and-sass', :description => 'Coursework for the "Advanced CSS and Sass" course')
   o.project('course-webgl', :description => 'Coursework for the "The Extensive WebGL Series ! - Part1 : Low Level Graphics" course')
+  o.project('course-pcg-roguelike', :description => 'Code written while working through "Procedural Content Generation for C++ Game Development"')
 
   # External projects that have been forked to submit pull requests
   o.project('endless-sky', :description => 'Space exploration, trading, and combat game.', :tags => %w(external travis homepage=http://endless-sky.github.io/))
