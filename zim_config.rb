e6289b5f9bd0ce90c317bfada388d7e993f35cb9
Peter Donald <peter@realityforge.org>
2019-10-17 08:52:02 +1100

    Bump spytools

--- zim_config.rb
+++ zim_config.rb
@@ -53,7 +53,7 @@ command(:patch_symbolmap_version) do |app|
 end
 
 command(:patch_arez_spytools_version) do |app|
-  patch_versions(app, %w(org.realityforge.arez.spytools:arez-spytools:jar), '0.74')
+  patch_versions(app, %w(org.realityforge.arez.spytools:arez-spytools:jar), '0.76')
 end
 
 command(:patch_revapi_version) do |app|
