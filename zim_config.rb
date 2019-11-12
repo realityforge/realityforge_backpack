028674226620fc53a37a70992d84f5ebf615d6c2
Peter Donald <peter@realityforge.org>
2019-11-12 13:38:35 +1100

    Bump elemental2

--- zim_config.rb
+++ zim_config.rb
@@ -124,8 +124,7 @@ command(:upgrade_elemental2) do |app|
     org.realityforge.com.google.elemental2:elemental2-webgl2:jar
     org.realityforge.com.google.elemental2:elemental2-webstorage:jar
     org.realityforge.com.google.elemental2:elemental2-webassembly:jar
-  ), '2.25')
-  # TODO: Can't move to 2.26 until GWT 2.8.3 is available.
+  ), '2.27')
 end
 
 command(:upgrade_arez) do |app|
