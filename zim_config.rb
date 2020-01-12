e900ee50b4b458cab4a05285e1d2e3e3f14457c1
Peter Donald <peter@realityforge.org>
2020-01-13 10:13:22 +1100

    Bump arez-spytools

--- zim_config.rb
+++ zim_config.rb
@@ -144,7 +144,7 @@ command(:patch_arez_version) do |app|
 end
 
 command(:patch_arez_spytools_version) do |app|
-  patch_versions(app, %w(org.realityforge.arez.spytools:arez-spytools:jar), '0.91')
+  patch_versions(app, %w(org.realityforge.arez.spytools:arez-spytools:jar), '0.92')
 end
 
 command(:patch_react4j_version) do |app|
