6fd8f66b90e5376d58e8abfe51a439ae21a54ffa
Peter Donald <peter@realityforge.org>
2020-01-29 10:48:13 +1100

    Fix description

--- scopes/sting-ioc.rb
+++ scopes/sting-ioc.rb
@@ -1,7 +1,7 @@
 Belt.scope('sting-ioc') do |o|
   o.project('sting', :description => 'A fast, easy to use, compile-time dependency injection toolkit', :tags => %w(codecov travis issues homepage=homepage=https://sting-ioc.github.io))
   # TODO: sting-ioc.github.io has several custom deploy keys. Figure out a way to automate this?
-  o.project('sting-ioc.github.io', :description => 'Arez website', :tags => %w(pages homepage=https://sting-ioc.github.io zim=no))
+  o.project('sting-ioc.github.io', :description => 'Sting website', :tags => %w(pages homepage=https://sting-ioc.github.io zim=no))
 
   o.projects.each do |project|
     project.tags << "name=#{project.name}"
