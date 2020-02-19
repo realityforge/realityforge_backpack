880ca99debd1399f52d7be3b919b3b9a7b10840d
Peter Donald <peter@realityforge.org>
2020-02-19 11:04:50 +1100

    Remove raw_bazel variant

--- scopes/react4j.rb
+++ scopes/react4j.rb
@@ -2,7 +2,7 @@ Belt.scope('react4j') do |o|
   o.project('react4j', :description => 'An opinionated react java binding', :tags => %w(travis issues homepage=https://react4j.github.io))
   # TODO: react4j.github.io has several custom deploy keys. Figure out a way to automate this?
   o.project('react4j.github.io', :description => 'React4j website', :tags => %w(pages homepage=https://react4j.github.io zim=no))
-  o.project('react4j-todomvc', :description => 'React4j TodoMVC implementation', :tags => %w(travis homepage=https://react4j.github.io/docs/todomvc.html zim:branches=raw,arez,dagger,sting,spritz,raw_bazel,sting_bazel,raw_bazel_j2cl,sting_maven,sting_maven_j2cl))
+  o.project('react4j-todomvc', :description => 'React4j TodoMVC implementation', :tags => %w(travis homepage=https://react4j.github.io/docs/todomvc.html zim:branches=raw,arez,dagger,sting,spritz,sting_bazel,raw_bazel_j2cl,sting_maven,sting_maven_j2cl))
   o.project('react4j-flux-challenge', :description => 'React4j Flux Challenge implementation', :tags => %w(travis))
   o.project('react4j-drumloop', :description => 'A React4j experimental drum machine', :tags => %w(travis))
 
