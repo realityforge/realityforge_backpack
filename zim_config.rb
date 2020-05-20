9eb6a3da0f91b5b399bf3cac6782e0c83296e88a
Peter Donald <peter@realityforge.org>
2020-05-21 09:22:51 +1000

    Patch proton

--- zim_config.rb
+++ zim_config.rb
@@ -96,7 +96,7 @@ command(:patch_getopt4j_version) do |app|
 end
 
 command(:patch_proton_version) do |app|
-  patch_versions(app, %w(org.realityforge.proton:proton-core:jar org.realityforge.proton:proton-qa:jar), '0.48')
+  patch_versions(app, %w(org.realityforge.proton:proton-core:jar org.realityforge.proton:proton-qa:jar), '0.49')
 end
 
 command(:patch_compile_testing_version) do |app|
