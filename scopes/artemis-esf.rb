c31824b134333a5a2af5d5d6b36256f76ec91042
Peter Donald <peter@realityforge.org>
2020-12-17 14:00:00 +1100

    Remove repos no longer care for

--- scopes/artemis-esf.rb
+++ scopes/artemis-esf.rb
@@ -1,8 +1,5 @@
 Belt.scope('artemis-esf') do |o|
   o.project('artemis-framework', :description => 'A mirror of https://code.google.com/p/artemis-framework')
-  o.project('GLGE', :description => 'A javascript webGL graphics library')
-  o.project('open3mod', :description => 'Open 3D Model Viewer - A quick and powerful 3D model viewer')
   o.project('noa', :description => 'Experimental voxel game engine.')
   o.project('proxx', :description => 'A game of proximity')
-  o.project('md2-view', :description => 'A MD2 model viewer')
 end
