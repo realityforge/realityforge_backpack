2d1e4b9d5990dd550db90fa83a1013a0f685081a
Peter Donald <peter@realityforge.org>
2021-01-07 10:48:46 +1100

    Bump zemeckis version

--- zim_config.rb
+++ zim_config.rb
@@ -84,7 +84,7 @@ command(:patch_jetbrains_annotations_version) do |app|
 end
 
 command(:patch_zemeckis_version) do |app|
-  patch_versions(app, %w(org.realityforge.zemeckis:zemeckis-core:jar), '0.01')
+  patch_versions(app, %w(org.realityforge.zemeckis:zemeckis-core:jar), '0.05')
 end
 
 command(:patch_guiceyloops_version) do |app|
