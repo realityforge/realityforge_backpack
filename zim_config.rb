8bdd2ac14e94346970cc4972e8328fa3c648e72d
Peter Donald <peter@realityforge.org>
2021-03-22 22:47:27 +1100

    Add akasha upgrade task

--- zim_config.rb
+++ zim_config.rb
@@ -37,6 +37,7 @@ command(:patch_jsinterop_version) do |app|
   patch_versions(app, %w(com.google.jsinterop:jsinterop-annotations:jar), version)
 end
 
+patch_artifact(:akasha, %w(org.realityforge.akasha:akasha-java:jar), '0.01')
 patch_artifact(:arez, %w(org.realityforge.arez:arez-core:jar org.realityforge.arez:arez-processor:jar), '0.183')
 patch_artifact(:arez_dom, %w(org.realityforge.arez.dom:arez-dom:jar), '0.70')
 patch_artifact(:arez_spytools, %w(org.realityforge.arez.spytools:arez-spytools:jar), '0.110')
