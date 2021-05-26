a86b49438481ee986220e1ea086fe06e0e6bc159
Peter Donald <peter@realityforge.org>
2021-05-27 09:00:24 +1000

    Correct description

--- scopes/akasha.rb
+++ scopes/akasha.rb
@@ -1,4 +1,4 @@
 Belt.scope('akasha') do |o|
   o.project('akasha', :description => 'Typed Browser APIs', :tags => %w(travis issues))
-  o.project('akasha-java', :description => 'A repository containing the source of the akasha-java artifact')
+  o.project('akasha-java', :description => 'A repository containing the source of the akasha-j2cl artifact')
 end
