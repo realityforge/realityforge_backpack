8d694da1505ebde9ba7a953bb3f6fe53e034d49a
Peter Donald <peter@realityforge.org>
2021-06-22 09:36:47 +1000

    Enable wiki

--- scopes/akasha.rb
+++ scopes/akasha.rb
@@ -1,4 +1,4 @@
 Belt.scope('akasha') do |o|
-  o.project('akasha', :description => 'Typed Browser APIs', :tags => %w(travis issues))
+  o.project('akasha', :description => 'Typed Browser APIs', :tags => %w(travis issues wiki))
   o.project('akasha-java', :description => 'A repository containing the source of the akasha-j2cl artifact')
 end
