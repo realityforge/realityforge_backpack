b371812b671d2153c8f33a46ccb960bf64b92809
Peter Donald <peter@realityforge.org>
2022-05-30 15:20:50 +1000

    Add projects keep for reference purposes that are still active

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -81,6 +81,13 @@ Belt.scope('realityforge') do |o|
   o.project('j2cl', :description => 'Java to closure style javascript transpiler', :tags => %w(external travis))
   o.project('closure-compiler', :description => 'A JavaScript checker and optimizer.', :tags => %w(external homepage=https://developers.google.com/closure/compiler/ travis))
 
+  # Projects that purely exist so that I can keep a copy of source for reference purposes that are still active
+  o.project('Quake2Game', :description => 'A WIP fork of Quake 2 intended to add features more commonly seen in modern game engines.', :tags => %w(external))
+  o.project('planet_quake', :description => 'Combination of graphics and features from many different games.', :tags => %w(external))
+  o.project('planet_quake_game', :description => 'Game QVMs for many different game dynamics.', :tags => %w(external))
+  o.project('RBDOOM-3-BFG', :description => 'Doom 3 BFG Edition with modern engine features (2021) like PBR, Baked Global Illumination, Soft Shadows, Cleaned up source, Linux and 64 bit Support', :tags => %w(external))
+  o.project('recastnavigation', :description => 'Navigation-mesh Toolset for Games', :tags => %w(external))
+
   # Projects that purely exist so that I can keep a copy of source for reference purposes
   o.project('3zb2', :description => '3rd Zigock Bot II for Yamagi Quake II', :tags => %w(external historic))
   o.project('OpenWolf-Engine', :description => 'Heavy modified idTech3 engine', :tags => %w(external historic))
