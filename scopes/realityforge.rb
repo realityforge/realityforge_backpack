7d2267970725700ac53a2b73e5d155855c4d07f5
Peter Donald <peter@realityforge.org>
2022-11-16 23:26:44 +1100

    Record a new repo

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -80,6 +80,7 @@ Belt.scope('realityforge') do |o|
   # Courses that are ongoing or still used for experiments
   o.project('course-webgl', :description => 'Coursework for learning WebGL')
   o.project('course-unreal-engine-the-ultimate-shooter', :description => 'Course work for "Unreal Engine C++ The Ultimate Shooter Course"', :tags => %w(default_branch=main))
+  o.project('course-blender-fundamentals-modeling', :description => 'Course work for "Blender Fundamentals - Modeling"', :tags => %w(default_branch=main))
   o.project('glfw-test', :tags => %w(default_branch=main))
 
   # Personal Music Exploration
