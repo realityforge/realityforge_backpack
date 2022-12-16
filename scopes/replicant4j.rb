a1f6313e49e8762b9666068ce00bba4b7e7fce11
Peter Donald <peter@realityforge.org>
2022-12-16 11:08:11 +1100

    Configure topics

--- scopes/replicant4j.rb
+++ scopes/replicant4j.rb
@@ -1,3 +1,3 @@
 Belt.scope('replicant4j') do |o|
-  o.project('replicant', :description => 'Client-side state representation infrastructure for GWT', :tags => %w(codecov notify:stock travis issues))
+  o.project('replicant', :description => 'Client-side state representation infrastructure for GWT', :tags => %w(codecov notify:stock topics=entity,gwt,java,network travis issues))
 end
