d8e89a4f6619626d296063a2f96ea0240c6234ec
Peter Donald <peter@realityforge.org>
2022-12-16 11:15:33 +1100

    Configure topics

--- scopes/sting-ioc.rb
+++ scopes/sting-ioc.rb
@@ -1,4 +1,4 @@
 Belt.scope('sting-ioc') do |o|
-  o.project('sting', :description => 'A fast, easy to use, compile-time dependency injection toolkit', :tags => %w(codecov travis issues homepage=https://sting-ioc.github.io))
+  o.project('sting', :description => 'A fast, easy to use, compile-time dependency injection toolkit', :tags => %w(codecov travis issues topics=codegen,dependency-injection,ioc,ioc-container,ioc-framework,java,sting homepage=https://sting-ioc.github.io))
   o.project('sting-ioc.github.io', :description => 'Sting website', :tags => %w(pages homepage=https://sting-ioc.github.io zim=no))
 end
