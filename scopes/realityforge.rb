4beec43059777cccd733caf346ccaaf6153f0dea
Peter Donald <peter@realityforge.org>
2023-08-15 09:58:53 +1000

    Register way_of_realityforge repository

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -5,6 +5,8 @@ Belt.scope('realityforge') do |o|
   o.project('gla', :description => 'An OpenGL Loading Library.')
   o.project('braincheck-c', :description => 'A tiny C library to verify invariants and simplify debugging without leaving a trace in production code.', :tags => %w(default_branch=main))
 
+  o.project('way_of_realityforge', :tags => %w(homepage=https://realityforge.org/way_of_realityforge/))
+
   o.project('buildr', :description => 'A stripped down fork of Buildr that works in modern environments.')
   o.project('buildr_plus', :description => 'A simple set of extensions that patch and customize buildr to our requirements.', :tags => %w(notify:stock))
   o.project('dbt', :description => 'A simple tool designed to simplify the creation, migration and deletion of databases.', :tags => %w(notify:stock travis pages))
