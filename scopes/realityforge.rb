a00c346bd1629d22998d5a889bd317d103d883d6
Peter Donald <peter@realityforge.org>
2024-04-12 17:23:10 +1000

    Record course-hard-surface-modeling-jumpstart

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -80,6 +80,7 @@ Belt.scope('realityforge') do |o|
   o.project('knife-cookbook-doc', :description => 'Knife plugin to document cookbooks', :tags => %w(travis pages issues))
 
   # Courses that are ongoing or still used for experiments
+  o.project('course-hard-surface-modeling-jumpstart', :description => 'Coursework for "Hard Surface Modeling Jumpstart"', :tags => %w(default_branch=main zim=no))
   o.project('course-creating-custom-decals-for-games', :description => 'Coursework for "Creating Custom Decals for Games"', :tags => %w(default_branch=main zim=no))
   o.project('course-art-tools-using-geometry-nodes', :description => 'Coursework for "Creating Art Tools using Blender Geometry Nodes"', :tags => %w(default_branch=main zim=no))
   o.project('course-unreal-engine-5-cpp-multiplayer-shooter', :description => 'Coursework for "Unreal Engine 5 C++ Multiplayer Shooter"', :tags => %w(default_branch=main))
