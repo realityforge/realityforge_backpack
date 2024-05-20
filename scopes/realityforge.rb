621ce1963bec602f64e56a07f44a887fac521bb9
Peter Donald <peter@realityforge.org>
2024-05-20 15:09:52 +1000

    Archive course-controlrig and course-hard-surface-modeling-jumpstart

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -83,9 +83,7 @@ Belt.scope('realityforge') do |o|
 
   # Courses that are ongoing or still used for experiments
   o.project('course-blender-complete-animator', :description => 'Coursework for "Blender Animation & Rigging: Bring Your Creations To Life"', :tags => %w(default_branch=main zim=no))
-  o.project('course-controlrig', :description => 'Coursework for "Control Rig for Unreal Engine 5"', :tags => %w(zim=no))
   o.project('course-creating-custom-decals-for-games', :description => 'Coursework for "Creating Custom Decals for Games"', :tags => %w(default_branch=main zim=no))
-  o.project('course-hard-surface-modeling-jumpstart', :description => 'Coursework for "Hard Surface Modeling Jumpstart"', :tags => %w(default_branch=main zim=no))
   o.project('course-sci-fi-weapon-design', :description => 'Coursework for "SciFi Weapon Design in Blender"', :tags => %w(default_branch=main zim=no))
   o.project('course-art-tools-using-geometry-nodes', :description => 'Coursework for "Creating Art Tools using Blender Geometry Nodes"', :tags => %w(default_branch=main zim=no))
   o.project('course-unreal-engine-5-cpp-multiplayer-shooter', :description => 'Coursework for "Unreal Engine 5 C++ Multiplayer Shooter"', :tags => %w(default_branch=main zim=no))
@@ -194,6 +192,8 @@ Belt.scope('realityforge') do |o|
   o.project('GraphicShaders', :description => 'A code repository of the Graphic Shaders book', :tags => %w(historic))
   o.project('Fabmedical', :description => 'Workshop for Azure container repository', :tags => %w(historic))
   o.project('wasm-contract-experiments', :description => 'Experiments using eWasm and Parity', :tags => %w(historic))
+  o.project('course-controlrig', :description => 'Coursework for "Control Rig for Unreal Engine 5"', :tags => %w(zim=no historic))
+  o.project('course-hard-surface-modeling-jumpstart', :description => 'Coursework for "Hard Surface Modeling Jumpstart"', :tags => %w(default_branch=main zim=no historic))
   o.project('course-advanced-trim-sheets', :description => 'Coursework for "Creating Advanced Trim Sheet Textures for Games"', :tags => %w(default_branch=main zim=no historic))
   o.project('course-blendertutorial', :description => 'Coursework for "Complete Blender Creator: Learn 3D Modelling for Beginners"', :tags => %w(default_branch=main historic))
   o.project('course-rigging-basics-for-games', :description => 'Coursework for "Rigging 101 For Games | Maya 2020"', :tags => %w(default_branch=main historic))
