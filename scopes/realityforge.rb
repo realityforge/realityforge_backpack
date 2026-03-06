7641ca79ff798daa3d0b727fc9e9dd998c377cb1
Peter Donald <peter@realityforge.org>
2026-03-06 13:32:38 +1100

    Archive graphql-java-scalars

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -43,7 +43,6 @@ Belt.scope('realityforge') do |o|
   o.project('keycloak-jaxrs-client-authfilter', :description => 'Filter for accessing keycloak secured services', :tags => %w(travis))
   o.project('keycloak-converger', :description => 'Converge the state of a keycloak realm', :tags => %w(travis))
   o.project('keycloak-domgen-support', :description => 'KeyCloak Domgen Support', :tags => %w(travis))
-  o.project('graphql-java-scalars', :description => 'graphql-java scalar types', :tags => %w(travis))
   o.project('glassfish-domain-patcher', :description => 'GlassFish Domain Patcher', :tags => %w(travis))
   o.project('glassfish-timers', :description => 'GlassFish timers database sql', :tags => %w(travis))
 
@@ -345,6 +344,7 @@ Belt.scope('realityforge') do |o|
 
   # Historic: Domgen updated to decouple from library
   o.project('graphql-domgen-support', :description => 'GraphQL Domgen Support', :tags => %w(historic))
+  o.project('graphql-java-scalars', :description => 'graphql-java scalar types', :tags => %w(historic))
 
   # Historic: Example inlined into source project
   o.project('gwt-cache-filter-example', :description => 'A simple application demonstrating the use of the gwt-cache-filter library', :tags => %w(historic))
