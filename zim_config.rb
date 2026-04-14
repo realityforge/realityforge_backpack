59e9146dfa0087ada9aee92259afe50344f9e5b4
Peter Donald <peter@realityforge.org>
2026-04-14 13:31:48 +1000

    Remove historic tasks

--- zim_config.rb
+++ zim_config.rb
@@ -25,22 +25,11 @@ patch_gem('mcrt', %w(1.9.0 1.10.0 1.11.0 1.12.0 1.13.0 1.14.0), '1.15.0')
 patch_gem('reality-mash', %w(1.9.0 1.10.0 1.11.0 1.12.0 1.13.0 1.14.0), '1.1.0')
 patch_gem('tiny_tds', %w(1.0.5), '2.1.3')
 
-command(:patch_jsinterop_version) do |app|
-  version = '2.0.0'
-  patch_dependency_coordinates(app, { 'org.realityforge.com.google.jsinterop:jsinterop-annotations:jar' => 'com.google.jsinterop:jsinterop-annotations:jar' }, version)
-  patch_versions(app, %w(com.google.jsinterop:jsinterop-annotations:jar), version)
-end
-
 patch_artifact(:akasha, %w(org.realityforge.akasha:akasha-gwt:jar org.realityforge.akasha:akasha-j2cl:jar), '0.30')
 patch_artifact(:arez, %w(org.realityforge.arez:arez-core:jar org.realityforge.arez:arez-processor:jar org.realityforge.arez:arez-extras-dom:jar org.realityforge.arez:arez-extras-promise:jar org.realityforge.arez:arez-extras-spytools:jar org.realityforge.arez:arez-extras-testng:jar), '0.231')
 patch_artifact(:braincheck, %w(org.realityforge.braincheck:braincheck-core:jar org.realityforge.braincheck:braincheck-testng:jar), '1.32.0')
-patch_artifact(:galdr, %w(org.realityforge.galdr:galdr-core:jar org.realityforge.galdr:galdr-processor:jar), '0.03')
 patch_artifact(:getopt4j, %w(org.realityforge.getopt4j:getopt4j:jar), '1.3')
 patch_artifact(:grim, %w(org.realityforge.grim:grim-annotations:jar org.realityforge.grim:grim-asserts:jar org.realityforge.grim:grim-processor:jar), '0.09')
-patch_artifact(:guava, %w(com.google.guava:guava:jar), '27.1-jre')
-patch_artifact(:guiceyloops, %w(org.realityforge.guiceyloops:guiceyloops:jar), '0.113')
-patch_artifact(:gwt_cache_filter, %w(org.realityforge.gwt.cache-filter:gwt-cache-filter:jar), '0.9')
-patch_artifact(:idea_codestyle, %w(au.com.stocksoftware.idea.codestyle:idea-codestyle:xml), '1.17')
 patch_artifact(:javaee_api, %w(javax:javaee-api:jar), '8.0')
 patch_artifact(:javapoet, %w(com.squareup:javapoet:jar), '1.13.0')
 patch_artifact(:javax_annotation, %w(org.realityforge.javax.annotation:javax.annotation:jar), '1.1.1')
@@ -51,6 +40,5 @@ patch_artifact(:revapi, %w(org.realityforge.revapi.diff:revapi-diff:jar:all), '0
 patch_artifact(:sting, %w(org.realityforge.sting:sting-core:jar org.realityforge.sting:sting-processor:jar), '0.29')
 patch_artifact(:symbolmap, %w(org.realityforge.gwt.symbolmap:gwt-symbolmap:jar), '0.09')
 patch_artifact(:zemeckis, %w(org.realityforge.zemeckis:zemeckis-core:jar), '0.14')
-patch_artifact(:vecmath, %w(org.realityforge.vecmath:vecmath:jar), '0.11')
 
 Zim::Belt.load_suites_from_belt
