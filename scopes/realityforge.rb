1b9c1f3db101fe314802503fd14cc2e0b0c07ebc
Peter Donald <peter@realityforge.org>
2026-05-12 11:42:31 +1000

    Archive timeservice

--- scopes/realityforge.rb
+++ scopes/realityforge.rb
@@ -55,7 +55,6 @@ Belt.scope('realityforge') do |o|
   o.project('gwt-symbolmap', :description => 'GWT SymbolMap Assertions Library.', :tags => %w(travis codecov))
   o.project('housekeeping-scripts', :description => 'Sets of scripts used to perform housekeeping at home and in the wild')
 
-  o.project('timeservice', :description => 'A simple EE service to get constant now within transaction', :tags => %w(travis))
   o.project('reality-core', :description => 'Basic classes used to help defining libraries.', :tags => %w(travis))
   o.project('reality-facets', :description => 'A basic toolkit for binding facets or extensions to model objects.', :tags => %w(travis))
   o.project('reality-generators', :description => 'A basic toolkit for abstracting the generation of files from model objects.', :tags => %w(travis))
@@ -132,6 +131,7 @@ Belt.scope('realityforge') do |o|
   o.project('proxy-servlet', :description => 'A servlet for creating proxy services', :tags => %w(historic))
   o.project('gwt-keycloak', :description => 'A simple library to provide keycloak support to GWT', :tags => %w(historic))
   o.project('keycloak-domgen-support', :description => 'KeyCloak Domgen Support', :tags => %w(historic))
+  o.project('timeservice', :description => 'A simple EE service to get constant now within transaction', :tags => %w(historic))
 
   # Historic: Inlined into the project that was using it
   o.project('gwt-serviceworker-linker', :description => 'A GWT linker that generates a serviceworker.', :tags => %w(historic))
